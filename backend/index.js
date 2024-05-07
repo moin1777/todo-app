703d21a7188aaca449c4c38bb0df465afa1abf2e
Moin Malek <malekmoin870@gmail.com>
2024-05-07 22:44:39 +0530

    added cors to the express server

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const {createTodo, updateTodo} = require("./types");
 const { todo } = require("./db");
 const app = express();
 
 const port = process.env.PORT || 3000;
 app.use(express.json());
+app.use(cors());
 
 app.post("/todos", async (req, res) => {
   const createPayload = req.body;
