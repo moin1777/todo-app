5d488bbc1ec10cf22008b56f2da1354c40323c79
Moin Malek <malekmoin870@gmail.com>
2024-05-10 00:12:46 +0530

    updated fetch call and todos component

--- frontend/src/components/Todos.jsx
+++ frontend/src/components/Todos.jsx
@@ -1,17 +1,19 @@
 import {useState} from "react";
-
+let globalId = 1;
 export default function Todos ({ todos }) {
   return (
     <div>
-      {todos.map((todo) => {
-        return (
-        <div>
-          <h1>{todo.title}</h1>
-          <h1>{todo.description}</h1>
-          <button>{todo.completed ? "Completed" : "Mark as compledted"}</button>
-        </div>
-        )
-      })}
+      {todos.map((todo) => <Todo key={globalId++} title={todo.title} description={todo.description} completed={todo.completed}></Todo>)}
+    </div>
+  )
+}
+
+function Todo ({title, description, completed}) {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <h1>{description}</h1>
+      <button>{completed ? "Completed" : "Mark as compledted"}</button>
     </div>
   )
 }
